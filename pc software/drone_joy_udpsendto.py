729758e30830a9357f788c99bd5fa9d4107b6d7f
dopro17 <douglas.prodocimo@dweiss.com.br>
2021-03-22 20:17:45 -0300

    Improvement in main program file name changed to drone.py to avoid inicialization on startup

--- pc software/drone_joy_udpsendto.py
+++ pc software/drone_joy_udpsendto.py
@@ -9,7 +9,10 @@ import pygame
 import time
 import socket
 import struct
+import psutil, os
 
+p = psutil.Process(os.getpid())
+p.nice(10)
 
 UDP_IP = "192.168.2.157"
 UDP_PORT = 5001
@@ -28,12 +31,15 @@ try:
     while True:
         event = pygame.event.get()
         pygame.event.clear()
-        eixos = (j.get_axis(0), j.get_axis(1))
-        print("\rEixo X: %+0.2f Eixo Y: %+0.2f" % eixos, end='')
+        eixos = (j.get_axis(3), j.get_axis(1))
+        if eixos[1] < 0.2 and eixos[1] > -0.2:
+            eixos = (eixos[0], 0.0)
+        
+        print("\rTração: %+0.2f Direção Y: %+0.2f" % eixos, end='')
         packet_data = packer.pack(*eixos)
         
         sock.sendto(packet_data,(UDP_IP, UDP_PORT))
-        time.sleep(0.05)
+        time.sleep(0.1)
 
 except KeyboardInterrupt:
     print("EXITING NOW")
